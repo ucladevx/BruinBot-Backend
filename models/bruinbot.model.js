9a4f849aa3bdaedfe300602c07b51c4a945708fd
dvorjackz <dvorjackz@gmail.com>
2020-11-17 21:29:48 -0800

    Cleaned code and added/modified event, bot, and item endpoints

--- models/bruinbot.model.js
+++ models/bruinbot.model.js
@@ -3,6 +3,19 @@ const map = require('./map.model.js');
 
 const schema = mongoose.Schema;
 
+// Represents an item and how many instances of it a bot holds
+const inventoryArticle = new schema({
+	item: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Item',
+		required: true,
+	},
+	quantity: {
+		type: Number,
+		default: 0,
+	},
+});
+
 const bruinBotSchema = new schema({
 	location: {
 		type: map.Location.schema,
@@ -20,8 +33,14 @@ const bruinBotSchema = new schema({
 		unique: true,
 		trim: true,
 	},
+	inventory: {
+		type: [inventoryArticle],
+		required: true,
+	},
 });
 
 const BruinBot = mongoose.model('BruinBot', bruinBotSchema);
+const InventoryArticle = mongoose.model('InventoryArticle', inventoryArticle);
 
-module.exports = BruinBot;
+module.exports.BruinBot = BruinBot;
+module.exports.InventoryArticle = InventoryArticle;
