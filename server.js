1c5b7af24d44e2c0fa401a236b0a665c6d2e3bb6
Thilan Tran <44995807+thilan-tran@users.noreply.github.com>
2020-11-29 20:33:56 -0800

    Add path routes (#25)
    * Add path routes
    
    * Remove route and simplify route urls
    
    * Readd missing file

--- server.js
+++ server.js
@@ -7,3 +7,20 @@ app.on('Mongoose ready', () => {
 		console.log(`This server is running on port ${port}!\n`);
 	});
 });
+
+const usersRouter = require('./routes/users');
+const botsRouter = require('./routes/bots');
+const itemsRouter = require('./routes/items');
+const eventsRouter = require('./routes/events');
+const pathsRouter = require('./routes/paths');
+
+app.get('/', (req, res) => {
+	res.json('Welcome to BruinBot API!');
+});
+app.use('/users', usersRouter);
+app.use('/bots', botsRouter);
+app.use('/items', itemsRouter);
+app.use('/events', eventsRouter);
+app.use('/paths', pathsRouter);
+
+module.exports = app;
