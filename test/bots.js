58d7cf41b501bf67b1e1c07f4d286de2add1f753
Jack Zhang <dvorjackz@gmail.com>
2020-12-03 05:14:04 -0800

    Add get bot by id endpoint (#28)

--- test/bots.js
+++ test/bots.js
@@ -32,7 +32,7 @@ before((done) => {
 	});
 });
 
-describe('BruinBot', () => {
+describe('Bot', () => {
 	suppressLogs();
 
 	// Start from fresh collection for each test
@@ -40,46 +40,79 @@ describe('BruinBot', () => {
 		await BruinBot.deleteMany({});
 	});
 
-	it('Should return list of all BruinBots', async () => {
-		await createAndSaveBot(exampleBotA);
-		await createAndSaveBot(exampleBotB);
+	describe('get /bots', () => {
+		it('Should return list of all BruinBots', async () => {
+			await createAndSaveBot(exampleBotA);
+			await createAndSaveBot(exampleBotB);
 
-		return await chai
-			.request(app)
-			.get('/bots')
-			.then((res) => {
-				assert.strictEqual(res.status, 200);
-				assert.ok(Array.isArray(res.body));
-				assert.strictEqual(res.body.length, 2);
-			});
+			return await chai
+				.request(app)
+				.get('/bots')
+				.then((res) => {
+					assert.strictEqual(res.status, 200);
+					assert.ok(Array.isArray(res.body));
+					assert.strictEqual(res.body.length, 2);
+				});
+		});
 	});
 
-	it('Should add a BruinBot', async () => {
-		return await chai
-			.request(app)
-			.post('/bots')
-			.send(exampleBotA)
-			.then(async (res) => {
-				assert.strictEqual(res.status, 200);
-				let bots = await BruinBot.find();
-				assert.strictEqual(bots.length, 1);
-			});
+	describe('get /bots/bot', () => {
+		it('Should return a specific BruinBot', async () => {
+			let botA = await createAndSaveBot(exampleBotA);
+
+			return await chai
+				.request(app)
+				.get('/bots/bot')
+				.send({ botId: botA._id })
+				.then((res) => {
+					assert.strictEqual(res.status, 200);
+					assert.strictEqual(exampleBotA.name, res.body.name);
+				});
+		});
+
+		it('Should return error status if BruinBot not found', async () => {
+			await createAndSaveBot(exampleBotA);
+
+			return await chai
+				.request(app)
+				.get('/bots/bot')
+				.send({ botId: 'clearlyWrongId' })
+				.then((res) => {
+					assert.strictEqual(res.status, 404);
+				});
+		});
 	});
 
-	it('Should delete a BruinBot', async () => {
-		let savedBot = await createAndSaveBot(exampleBotA);
+	describe('post /bots', () => {
+		it('Should add a BruinBot', async () => {
+			return await chai
+				.request(app)
+				.post('/bots')
+				.send(exampleBotA)
+				.then(async (res) => {
+					assert.strictEqual(res.status, 200);
+					let bots = await BruinBot.find();
+					assert.strictEqual(bots.length, 1);
+				});
+		});
+	});
 
-		return await chai
-			.request(app)
-			.delete('/bots')
-			.send({
-				id: savedBot._id,
-			})
-			.then(async (res) => {
-				assert.strictEqual(res.status, 200);
-				let bots = await BruinBot.find();
-				assert.strictEqual(bots.length, 0);
-			});
+	describe('delete /bots', () => {
+		it('Should delete a BruinBot', async () => {
+			let savedBot = await createAndSaveBot(exampleBotA);
+
+			return await chai
+				.request(app)
+				.delete('/bots')
+				.send({
+					id: savedBot._id,
+				})
+				.then(async (res) => {
+					assert.strictEqual(res.status, 200);
+					let bots = await BruinBot.find();
+					assert.strictEqual(bots.length, 0);
+				});
+		});
 	});
 });
 
