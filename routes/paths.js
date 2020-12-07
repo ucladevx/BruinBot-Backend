b946281e22a05c1b0f2d9faf374ec42fbf51f883
Zack Hirschhorn <zhirschhorn@gmail.com>
2020-12-06 23:15:46 -0800

    basic a* working

--- routes/paths.js
+++ routes/paths.js
@@ -3,6 +3,172 @@ const express = require('express');
 const mapRouter = express.Router();
 
 const { Location, MapNode, Path } = require('../models/map.model');
+const { coordDistanceM } = require('./utils');
+
+const getEuclidianDistance = (node1, node2) => {
+	return Math.sqrt(Math.pow((node1.location.latitude - node2.location.latitude),2)+Math.pow((node1.location.longitude - node2.location.longitude),2));
+}
+
+const heuristic = (node, endpoint) => {
+	//return coordDistanceM(path.node.latitude, path.nodeB.longitude, endpoint.longitude, endpoint.latitude);
+	//const nodeB = await MapNode.findById(path.nodeB);
+	//console.log(node);
+	return Math.abs(node.location.latitude - endpoint.location.latitude)+Math.abs(node.location.longitude - endpoint.location.longitude);
+}
+
+const getClosestMapNode = async (latitude, longitude) => {
+	//Might be slow
+	const curLocation = new MapNode({ location: {
+		latitude: latitude, 
+		longitude: longitude
+	}});
+	const mapNodes = await MapNode.find();
+	let closestNode = mapNodes[0];
+	let closestDist = getEuclidianDistance(mapNodes[0], curLocation);
+	for (let node of mapNodes){
+		if (getEuclidianDistance(node, curLocation) < closestDist){
+			closestDist = getEuclidianDistance(node, curLocation);
+			closestNode = node;
+		}
+	} 
+	//console.log(closestNode);
+	return closestNode;
+}
+
+const getNeighbors = async (node) => {
+	let nodes = [];
+	const pathsA = await Path.find({ nodeA: node });
+	for (let path of pathsA){
+		const nodeB = await MapNode.findById(path.nodeB);
+		nodes.push(nodeB);
+	}
+	const pathsB = await Path.find({ nodeB: node });
+	for (let path of pathsB){
+		const nodeA = await MapNode.findById(path.nodeA);
+		nodes.push(nodeA);
+	}
+	//console.log(nodes);
+	return nodes;
+}
+
+const reconstructPath = async (cameFrom, curNode) => {
+	let locations = [curNode.location];
+	while (cameFrom.has(curNode)){
+		nextNode = cameFrom.get(curNode);
+		points = await getPoints(curNode, nextNode);
+		for (let p of points){
+			locations.unshift(p);
+		}
+		locations.unshift(nextNode.location);
+		curNode = nextNode;
+	}
+	return locations;
+}
+
+const getPoints = async (node1, node2) => {
+	let path = await Path.findOne({
+		'nodeA': node1,
+		'nodeB': node2
+	});
+	let points = [];
+	//console.log(path);
+	if (path === null){
+		path = await Path.findOne({
+			'nodeA': node2,
+			'nodeB': node1
+		});
+		points = path.points.reverse();
+	} else {
+		points = path.points;
+	}
+	//console.log(path);
+	return points;
+}
+
+mapRouter.route('/pathBetween').get(async (req, res) => {
+	let startNode = await MapNode.findOne({
+		'location.latitude': req.body.startLat,
+		'location.longitude': req.body.startLon,
+	});
+	if (startNode === null){
+		startNode = await getClosestMapNode(req.body.startLat, req.body.startLon)
+	}
+	let endNode = await MapNode.findOne({
+		'location.latitude': req.body.endLat,
+		'location.longitude': req.body.endLon,
+	});
+	if (endNode === null){
+		endNode = await getClosestMapNode(req.body.endLat, req.body.endLon)
+	}
+	const nodes = await getPathBetween(startNode, endNode);
+	//console.log(nodes);
+	res.json(nodes);
+});
+
+const alreadyHas = (node, set) => {
+	//console.log(node)
+	//console.log(set);
+	for (let s of set){
+		if (node.location.latitude === s.location.latitude && node.location.longitude === s.location.longitude){
+			return true;
+		}
+	}
+	return false;
+}
+
+
+const getPathBetween = async (start, end) => {
+	let gScore = new Map();
+	let fScore = new Map();
+	let cameFrom = new Map();
+	let queue = new Set();
+	let alreadyVisited = new Set();
+
+	gScore.set(start, 0);
+	fScore.set(start, heuristic(start, end));
+	queue.add(start);
+
+	while (queue.size !== 0){
+		let curNode;
+		let smallestF = Number.MAX_SAFE_INTEGER;
+
+		for (let node of queue){
+			//console.log(fScore.get(node));
+			if (fScore.get(node) < smallestF){
+				curNode = node;
+				smallestF = fScore.get(node);
+			}
+		}
+		console.log(curNode);
+		queue.delete(curNode);
+		alreadyVisited.add(curNode);
+		if (curNode.location.latitude === end.location.latitude && curNode.location.longitude === end.location.longitude){
+			return await reconstructPath(cameFrom, curNode);
+		}
+		let neighbors = await getNeighbors(curNode);
+		for (let neighbor of neighbors){
+			const points = await getPoints(curNode, neighbor);
+			let dist = points.length + 1;
+			//console.log(dist)
+			const tempG = dist + gScore.get(curNode);
+			if (gScore.get(neighbor) === undefined || tempG < gScore.get(neighbor)){
+				gScore.set(neighbor, tempG);
+				fScore.set(neighbor, tempG + heuristic(neighbor, end));
+				cameFrom.set(neighbor, curNode);
+				if (!alreadyHas(neighbor, queue) && !alreadyHas(neighbor, alreadyVisited)){
+					queue.add(neighbor);
+				}
+			}
+		}
+	}
+	return "No Path Found";
+}
+
+mapRouter.route('/resetPath').delete(async (req, res) => {
+	await Path.deleteMany({});
+	await MapNode.deleteMany({});
+	res.json("done");
+});
 
 /**
  * Get all map nodes.
