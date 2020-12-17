48fd9883a72060bb0812368b9a1e8fe10fd9dfbb
Zhirschhorn <Zhirschhorn@gmail.com>
2020-12-16 23:04:34 -0800

    Zack/user (#36)
    * use id token as opposed to id
    
    * formatting
    
    * add json as env variable
    
    * Add FIREBASE_KEY and FIREBASE_URL to Github Actions env
    
    Co-authored-by: dvorjackz <dvorjackz@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 let { User } = require('../models/user.model');
+let admin = require('firebase-admin');
+
+let jsonKey = process.env.FIREBASE_KEY;
+
+admin.initializeApp({
+	credential: admin.credential.cert(JSON.parse(jsonKey)),
+	databaseURL: process.env.FIREBASE_URL,
+});
 
 /**
  * ----------------- GET (return information about objects) ----------------
@@ -11,6 +19,7 @@ let { User } = require('../models/user.model');
 /**
  * Gets all the users
  */
+
 router.route('/').get((req, res) => {
 	User.find()
 		.then((users) => res.json(users))
@@ -22,19 +31,32 @@ router.route('/').get((req, res) => {
  */
 
 /**
- * Adds a new User object with the username and firebase_id provided in the
+ * Adds a new User object with the username and firebase_id_token provided in the
  * request body
  */
-router.route('/add').post((req, res) => {
-	const { username, firebase_id } = req.body;
+router.route('/add').post(async (req, res) => {
+	const { username, firebase_id_token } = req.body;
 
-	if (!firebase_id || !username)
+	if (!firebase_id_token || !username) {
 		return res
 			.status(400)
-			.json('Required username / firebase_id not provided in request body.');
+			.json(
+				'Required username / firebase_id_token not provided in request body.'
+			);
+	}
+
+	let uid;
+	try {
+		const decodedToken = await admin.auth().verifyIdToken(firebase_id_token);
+		uid = decodedToken.uid;
+	} catch (err) {
+		console.log('Error ' + err);
+		res.status(400).json(err);
+		return;
+	}
 
 	const newUser = new User({
-		firebase_id: firebase_id,
+		firebase_id: uid,
 		username: username,
 	});
 
@@ -49,33 +71,42 @@ router.route('/add').post((req, res) => {
  */
 
 /**
- * Adds/removes user given by firebase id as organizer based
+ * Adds/removes user given by firebase id token as organizer based
  * on the boolean passed in as isOrganizer
  */
 router.route('/organizer').put(async (req, res) => {
-	const { firebase_id, isOrganizer } = req.body;
+	const { firebase_id_token, isOrganizer } = req.body;
 
-	if (!firebase_id || isOrganizer === undefined)
+	if (!firebase_id_token || isOrganizer === undefined) {
 		return res
 			.status(400)
-			.json('Required firebase_id / isOrganizer not provided in request body.');
+			.json(
+				'Required firebase_id_token / isOrganizer not provided in request body.'
+			);
+	}
+
+	let uid;
+	try {
+		const decodedToken = await admin.auth().verifyIdToken(firebase_id_token);
+		uid = decodedToken.uid;
+	} catch (err) {
+		console.log('Error ' + err);
+		res.status(400).json(err);
+		return;
+	}
 
 	try {
-		let user = await User.findOne({ firebase_id: firebase_id });
+		let user = await User.findOne({ firebase_id: uid });
 
 		if (!user)
 			return res
 				.status(404)
-				.json('Could not find user specified by firebase_id.');
+				.json('Could not find user specified by firebase_id_token.');
 
 		user.isOrganizer = isOrganizer;
 		user.save();
-		if (isOrganizer)
-			res.json('User ' + req.body.firebase_id + ' was made organizer!');
-		else
-			res.json(
-				'User ' + req.body.firebase_id + ' was removed as an organizer!'
-			);
+		if (isOrganizer) res.json('User ' + uid + ' was made organizer!');
+		else res.json('User ' + uid + ' was removed as an organizer!');
 	} catch (err) {
 		console.log('Error: ' + err);
 		res.status(400).json(err);
@@ -87,26 +118,37 @@ router.route('/organizer').put(async (req, res) => {
  */
 
 /**
- * Deletes a user by firebase id
+ * Deletes a user by firebase id token
  */
 router.route('/').delete(async (req, res) => {
-	const { firebase_id } = req.body;
+	const { firebase_id_token } = req.body;
 
-	if (!firebase_id)
+	if (!firebase_id_token) {
 		return res
 			.status(400)
-			.json('Required firebase_id not provided in request body.');
+			.json('Required firebase_id_token not provided in request body.');
+	}
+
+	let uid;
+	try {
+		const decodedToken = await admin.auth().verifyIdToken(firebase_id_token);
+		uid = decodedToken.uid;
+	} catch (err) {
+		console.log('Error ' + err);
+		res.status(400).json(err);
+		return;
+	}
 
 	try {
-		let user = await User.findOne({ firebase_id: firebase_id });
+		let user = await User.findOne({ firebase_id: uid });
 
 		if (!user)
 			return res
 				.status(404)
-				.json('Could not find user specified by firebase_id.');
+				.json('Could not find user specified by firebase_id_token.');
 
 		await user.deleteOne();
-		res.json('User ' + firebase_id + ' was deleted!');
+		res.json('User ' + uid + ' was deleted!');
 	} catch (err) {
 		console.log('Error: ' + err);
 		res.status(400).json(err);
