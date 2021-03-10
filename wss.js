7f83f130b0d710382958f6756dbd8f18017e00d0
Aristotle Henderson <53080395+AristotleH@users.noreply.github.com>
2021-03-09 20:22:43 -0800

    Add bot location update handler with WebSockets (#65)
    * Add bot location update handler with WebSockets
    
    * Update WebSockets commands to all-lowercase text

--- wss.js
+++ wss.js
@@ -1,18 +1,50 @@
 const WebSocket = require('ws');
+let { BruinBot } = require('./models/bruinbot.model');
+let { coordDistanceM } = require('./util/utils');
+let { VICINITY } = require('./constants');
 
 const wss = new WebSocket.Server({ noServer: true });
 
+const updateLocationHandler = async function (botId, lat, lon) {
+	try {
+		let bot = await BruinBot.findById(botId);
+
+		if (!bot) return;
+
+		bot.location.latitude = lat;
+		bot.location.longitude = lon;
+
+		if (bot.status == 'InTransit') {
+			let pathEnd = bot.path[bot.path.length - 1];
+			if (
+				coordDistanceM(lat, lon, pathEnd.latitude, pathEnd.longitude) < VICINITY
+			) {
+				bot.path = [];
+				bot.status = 'Idle';
+			}
+		}
+
+		await bot.save();
+		console.log(`Successfully updated location of bot ${botId}`);
+	} catch (err) {
+		console.log('Error: ' + err);
+	}
+};
+
 const messageHandler = (msg) => {
-	console.log(`received: ${msg}`);
+	const msgSplit = msg.split(' ');
+	console.log(msg);
+	if (msgSplit[0] == 'location' && msgSplit.length == 4) {
+		updateLocationHandler(msgSplit[1], msgSplit[2], msgSplit[3]);
+		return 'Accepted and attempting location update request to database!';
+	} else if (msgSplit[0] == 'join') return 'Welcome to BruinBot!';
+	else return 'Error: WebSocket request not valid.';
 };
 
 wss.on('connection', (ws) => {
 	ws.on('message', (msg) => {
-		messageHandler(msg);
-		ws.send(`received: ${msg}`);
+		ws.send(messageHandler(msg));
 	});
-
-	ws.send('Welcome to BruinBot!');
 });
 
 module.exports.wss = wss;
